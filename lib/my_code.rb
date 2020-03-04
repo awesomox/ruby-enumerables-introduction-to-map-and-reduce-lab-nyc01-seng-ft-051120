c2c1104f051a95ec0814ecff0bc8f94725c0f128
tom hannaford <thom.hannaford@gmail.com>
2020-03-04 20:31:25 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,66 @@
-# My Code here....
+def map_to_negativize(array)
+  negative_array = []
+  row_index = 0 
+  while row_index < array.length do 
+    result = array[row_index] * -1
+    negative_array << result 
+    row_index += 1 
+  end
+  negative_array
+end
+
+def map_to_no_change(array)
+  new_array = array
+  new_array
+end
+
+def map_to_double(array)
+  doubled_array = []
+  row_index = 0 
+  while row_index < array.length do 
+    result = array[row_index] * 2
+    doubled_array << result 
+    row_index += 1 
+  end
+  doubled_array
+end
+
+def map_to_square(array)
+  squared_array = []
+  row_index = 0 
+  while row_index < array.length do 
+    result = array[row_index] * array[row_index]
+    squared_array << result 
+    row_index += 1 
+  end
+  squared_array
+end
+
+def reduce_to_total(array, starting_point = 0)
+  row_index = 0 
+  total = starting_point
+  while row_index < array.length do 
+    total += array[row_index]
+    row_index += 1 
+  end
+  return total 
+end
+
+def reduce_to_all_true(array)
+  if array.all? 
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+def reduce_to_any_true(array)
+  if array.any?
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+    
+    
