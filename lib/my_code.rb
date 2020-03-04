995bf9c058b303eab4c81905a3f76e62ad5a1aa8
tom hannaford <thom.hannaford@gmail.com>
2020-03-04 20:24:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,7 +49,7 @@ end
 def reduce_to_all_true(array)
   row_index = 0 
   while row_index < array.length do 
-    if array[row_index] == true 
+    if array[row_index] != false  
       row_index +=1 
     else
       return false 
